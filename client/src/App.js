274657cf89df38265ea2069c75b3ff1bd9754b14
Patty C <patrickcorcoran10@gmail.com>
2019-01-17 23:16:23 -0600

    commit that has the routing between pages working in the url field

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-// import Start from "./components/Start/Start";
-// import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import UserView from "./pages/UserView";
+import Form from "./pages/Form";
+import Visuals from "./pages/Visuals";
+import Signin from "./pages/Signin";
+// import Navbar from "./components/Navbar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Wrapper from "./components/Wrapper/Wrapper";
-import Parent from "./components/Parent/Parent";
+// import Parent from "./components/Parent/Parent";
 // import MultiyearROI from './components/MultiyearROI/MultiyearROI';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <Navbar /> */}
-          <Wrapper>
-            <Parent />
-            {/* <MultiyearROI /> */}
-          </Wrapper>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      {/* <Navbar /> */}
+      <Wrapper>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/form" component={Form} />
+        <Route exact path="/view" component={UserView} />
+        <Route exact path="/visuals" component={Visuals} />
+        <Route exact path="/signin" component={Signin} />
+      </Wrapper>
+      <Footer />
+    </div>
+  </Router>
+
+
+)
 
 export default App;
